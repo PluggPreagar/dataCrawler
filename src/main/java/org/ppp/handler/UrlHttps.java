01a4a532b1418a76d6c49231082cfa5a6c34cea2
pluggPreagar <pluggPreagar@preisfrieden.de>
2024-12-17 20:34:35 +0100

    i2

--- src/main/java/org/ppp/handler/UrlHttps.java
+++ src/main/java/org/ppp/handler/UrlHttps.java
@@ -1,8 +1,22 @@
 package org.ppp.handler;
 
+import org.ppp.Session;
+
+import java.util.List;
+
 public class UrlHttps extends Handler{
 
     public UrlHttps() {
         super( UrlHttps.class);
+    }
+
+    @Override
+    public List<List<String>> load(String query, Session session) {
+        return super.load(query, session);
+    }
+
+    @Override
+    public void save(List<List<String>> cells, String query, Session session) {
+        super.save(cells, query, session);
     }
 }
